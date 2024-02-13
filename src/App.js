798d165dac9a2f766495f85d2528c700e5c72d0b
keerthinanjundareddy <keerthana@apprikart.com>
2024-02-13 14:03:56 +0530

    fi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import routes from '../src/routes';
 
-function App() {
+import './assets/css/materialdesignicons.min.css';
+import './assets/scss/themes.scss';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Routes>
+          {routes.map((route, idx) => (
+            <Route path={route.path} element={<route.component />} key={idx} />
+          ))}
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
